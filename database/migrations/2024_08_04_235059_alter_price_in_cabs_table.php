c36df231fbf3a97b9f60abcfa6a747c7de381a26
Souvik3798 <souvikbarua693@gmail.com>
2024-08-05 04:53:59 +0530

    "Changed 'int' to 'integer' in migration for altering 'price' column in 'cabs' table"

--- database/migrations/2024_08_04_235059_alter_price_in_cabs_table.php
+++ database/migrations/2024_08_04_235059_alter_price_in_cabs_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('cabs', function (Blueprint $table) {
-            $table->int('price')->change();
+            $table->integer('price')->change();
         });
     }
 };
