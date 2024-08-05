2075e1c5f0a57764db4a9fe8c42c1a580cea2f3b
Souvik3798 <souvikbarua693@gmail.com>
2024-08-05 23:41:02 +0530

    sss

--- app/Filament/Resources/KindlynoteResource.php
+++ app/Filament/Resources/KindlynoteResource.php
@@ -22,6 +22,7 @@ class KindlynoteResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $navigationGroup = 'Policy';
     protected static ?string $label = 'Kindly Note';
+    protected static ?string $pluralLabel = 'Kindly Note';
 
     public static function form(Form $form): Form
     {
