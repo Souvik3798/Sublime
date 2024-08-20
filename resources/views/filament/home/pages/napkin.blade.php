140bad44bbef8de02d21cf54f6cbf57eac91d408
Souvik3798 <souvikbarua693@gmail.com>
2024-08-20 19:40:17 +0530

    "Updated iframe src in napkin.blade.php from signin to root URL"

--- resources/views/filament/home/pages/napkin.blade.php
+++ resources/views/filament/home/pages/napkin.blade.php
@@ -1,3 +1,3 @@
 <x-filament-panels::page>
-    <iframe src="https://app.napkin.ai/signin" frameborder="0" width="100%" height="600"></iframe>
+    <iframe src="https://app.napkin.ai/" frameborder="0" width="100%" height="600"></iframe>
 </x-filament-panels::page>
