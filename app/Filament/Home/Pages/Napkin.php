a9dab8ccd5deb567597d63cddc5f932052f66617
Souvik3798 <souvikbarua693@gmail.com>
2024-08-20 19:38:11 +0530

    new integration

---
+++ app/Filament/Home/Pages/Napkin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Home\Pages;
+
+use Filament\Pages\Page;
+
+class Napkin extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
+
+    protected static string $view = 'filament.home.pages.napkin';
+}
