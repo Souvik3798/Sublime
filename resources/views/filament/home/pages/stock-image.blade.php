30465d41466e9bd6b67ac8696c4282d7ad97e981
Souvik3798 <souvikbarua693@gmail.com>
2024-08-20 23:37:11 +0530

    "Updated iframe width and height attributes in stock-image.blade.php"

--- resources/views/filament/home/pages/stock-image.blade.php
+++ resources/views/filament/home/pages/stock-image.blade.php
@@ -1,4 +1,4 @@
 <x-filament-panels::page>
-    <iframe src="https://dinesh29-stabilityai-stable-diffusion-3-medium-diffusers.hf.space" frameborder="0" width="850"
-        height="450"></iframe>
+    <iframe src="https://dinesh29-stabilityai-stable-diffusion-3-medium-diffusers.hf.space" frameborder="0" width="10%"
+        height="auto"></iframe>
 </x-filament-panels::page>
