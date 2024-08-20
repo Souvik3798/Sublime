05d99c55c51c84341bfbc10fa9b10289e3e1e936
Souvik3798 <souvikbarua693@gmail.com>
2024-08-20 23:38:12 +0530

    "Updated iframe height in stock-image.blade.php from 'auto' to '600'"

--- resources/views/filament/home/pages/stock-image.blade.php
+++ resources/views/filament/home/pages/stock-image.blade.php
@@ -1,4 +1,4 @@
 <x-filament-panels::page>
     <iframe src="https://dinesh29-stabilityai-stable-diffusion-3-medium-diffusers.hf.space" frameborder="0" width="100%"
-        height="auto"></iframe>
+        height="600"></iframe>
 </x-filament-panels::page>
