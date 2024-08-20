15f2b866e845112bbfe2dde5953a67ff9192d4c0
Souvik3798 <souvikbarua693@gmail.com>
2024-08-20 23:36:11 +0530

    kjbskbjs

---
+++ app/Filament/Home/Pages/StockImage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Home\Pages;
+
+use Filament\Pages\Page;
+
+class StockImage extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+
+    protected static string $view = 'filament.home.pages.stock-image';
+}
---
+++ resources/views/filament/home/pages/stock-image.blade.php
@@ -0,0 +1,4 @@
+<x-filament-panels::page>
+    <iframe src="https://dinesh29-stabilityai-stable-diffusion-3-medium-diffusers.hf.space" frameborder="0" width="850"
+        height="450"></iframe>
+</x-filament-panels::page>
