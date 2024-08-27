748678b5d88f776f7fdd416350758a1e96596f39
Souvik3798 <souvikbarua693@gmail.com>
2024-08-27 10:19:22 +0530

    get navigation lkabel

--- app/Filament/Home/Pages/AI.php
+++ app/Filament/Home/Pages/AI.php
@@ -9,12 +9,10 @@ class AI extends Page
     protected static ?string $navigationIcon = 'heroicon-o-cpu-chip';
     protected static ?int $navigationSort = 2;
 
-    protected static ?string $label = 'AI Assistant';
-    protected static ?string $pluralLabel = 'AI Assistant';
-
-    protected static ?string $navigationLabel = 'AI Assistant';
-
-    protected static ?string $modelLabel = 'AI Assistant';
-
     protected static string $view = 'filament.home.pages.a-i';
+
+    public static function getNavigationLabel(): string
+    {
+        return '';
+    }
 }
