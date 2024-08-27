6212ad1eaa6a31ad5feb5b68a39b83f20a55ea91
Souvik3798 <souvikbarua693@gmail.com>
2024-08-27 10:28:17 +0530

    "Updated iframe layout in a-i.blade.php"

--- resources/views/filament/home/pages/a-i.blade.php
+++ resources/views/filament/home/pages/a-i.blade.php
@@ -1,3 +1,6 @@
 <x-filament-panels::page>
-    <iframe src="https://ai.nxtunicorn.com/" frameborder="0" width="100%" height="600"></iframe>
+    <div style="height: 100vh; margin: 0; padding: 0;">
+        <iframe src="https://ai.nxtunicorn.com/" frameborder="0" width="100%" height="100%"
+            style="height: 100%;"></iframe>
+    </div>
 </x-filament-panels::page>
