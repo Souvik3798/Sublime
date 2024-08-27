c06424af637991d76a455aa2678cf7b52a5d75f5
Souvik3798 <souvikbarua693@gmail.com>
2024-08-27 10:15:10 +0530

    "Updated iframe height in stock-image.blade.php from 'max' to '1000px'"

--- resources/views/filament/home/pages/stock-image.blade.php
+++ resources/views/filament/home/pages/stock-image.blade.php
@@ -1,4 +1,4 @@
 <x-filament-panels::page>
     <iframe src="https://dinesh29-stabilityai-stable-diffusion-3-medium-diffusers.hf.space" frameborder="0" width="100%"
-        height="max"></iframe>
+        height="1000px"></iframe>
 </x-filament-panels::page>
