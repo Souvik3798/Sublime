d8adaf458e09a2331ae86ac00fab4c634d9c3294
Souvik3798 <souvikbarua693@gmail.com>
2024-08-27 10:24:52 +0530

    "Update title property in AI page from 'Custom Page Title' to empty string"

--- app/Filament/Home/Pages/AI.php
+++ app/Filament/Home/Pages/AI.php
@@ -8,6 +8,6 @@ class AI extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-cpu-chip';
     protected static ?int $navigationSort = 2;
-    protected static ?string $title = 'Custom Page Title';
+    protected static ?string $title = '';
     protected static string $view = 'filament.home.pages.a-i';
 }
