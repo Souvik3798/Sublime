26cb6936962988d3ac791c61409caca9af9e3ce1
dineshlalgit <dineshlal29@gmail.com>
2025-07-08 21:36:14 +0530

    Add password hashing in user edit form

--- app/Filament/Owner/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Owner/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,7 @@ namespace App\Filament\Owner\Resources\UserResource\Pages;
 use App\Filament\Owner\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -19,5 +20,14 @@ class EditUser extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (!empty($data['new_password'])) {
+            $data['password'] = Hash::make($data['new_password']);
+        }
+        unset($data['new_password'], $data['new_password_confirmation']);
+        return $data;
     }
 }
