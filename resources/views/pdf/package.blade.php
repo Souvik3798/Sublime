fe3f508070d4dd0accb1aea8401eb693945a8963
dineshlalgit <dineshlal29@gmail.com>
2025-08-06 21:01:26 +0530

    dd

--- resources/views/pdf/package.blade.php
+++ resources/views/pdf/package.blade.php
@@ -1081,7 +1081,7 @@
                     @foreach ($record->cruz as $cruise)
                         <div class="cruise-card">
                             <div class="cruise-header">
-                                <?php dd($cruise->ferry); ?>
+                                <?php dd($cruise->ferry['title']); ?>
                                 <h3><i class="fas fa-ship"></i> {{ $cruise['Title'] ?? 'Cruise Name Not Available' }}</h3>
                             </div>
                             <div class="cruise-info">
