f2791fd9607c63e99f122a13fe5c4437126327d8
dineshlalgit <dineshlal29@gmail.com>
2025-08-06 21:37:52 +0530

    dd

--- resources/views/pdf/package.blade.php
+++ resources/views/pdf/package.blade.php
@@ -1081,12 +1081,7 @@
                     @foreach ($record->cruz as $cruise)
                         <div class="cruise-card">
                             <div class="cruise-header">
-                                <?php
-                                    foreach ($cruise as $cruises) {
-                                        $ferry = App\Models\Ferry::where('id', $cruises['ferry'])->first();
-                                        dd($ferry);
-                                    }
-                                    ?>
+                                <?php dd($cruise); ?>
                                 <h3><i class="fas fa-ship"></i> {{ $cruise['Title'] ?? 'Cruise Name Not Available' }}</h3>
                             </div>
                             <div class="cruise-info">
